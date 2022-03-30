c698c4689b60acb9ef8348a1161917bd2d250522
Andrew Gaffney <andrew@agaffney.org>
2022-03-30 18:59:15 -0500

    Initial commandline parsing, config loading, and logging support

---
+++ config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Config struct {
+	Logging LoggingConfig `yaml:"logging"`
+}
+
+type LoggingConfig struct {
+	Level string `yaml:"level" envconfig:"LOGGING_LEVEL" default:"info"`
+}
+
+// Singleton config instance
+var globalConfig *Config
+
+func Load(configFile string) (*Config, error) {
+	cfg := &Config{}
+	// Load config file as YAML if provided
+	if configFile != "" {
+		buf, err := ioutil.ReadFile(configFile)
+		if err != nil {
+			return nil, fmt.Errorf("error reading config file: %s", err)
+		}
+		err = yaml.Unmarshal(buf, cfg)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing config file: %s", err)
+		}
+	}
+	// Load config values from environment variables
+	// This also has the side effect of setting default values from annotations
+	err := envconfig.Process("", cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error processing environment: %s", err)
+	}
+	// Assign to global config var
+	globalConfig = cfg
+	return cfg, nil
+}
+
+func GetConfig() *Config {
+	return globalConfig
+}
